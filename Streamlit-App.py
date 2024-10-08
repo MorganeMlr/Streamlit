dbb45b886841f00a8a4facdadf5d544a73480747
MorganeMlr <mlr.morgane@gmail.com>
2024-10-08 19:24:53 +0200

    Update Streamlit-App.py

--- Streamlit-App.py
+++ Streamlit-App.py
@@ -195,8 +195,8 @@ if page == pages[3] :
     
     if option == 'La visualisation des données':
        # Afficher la première image
-                  image_path = "Distribution des salaires par régions.png"
-                  st.image(image_path, caption='Distribution des salaires par région', use_column_width=True)
+       image_path = "Distribution des salaires par régions.png"
+       st.image(image_path, caption='Distribution des salaires par région', use_column_width=True)
     
     if option == 'La matrice de corrélation':
        # Afficher l'image 
